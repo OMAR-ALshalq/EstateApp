ef05e7e841e74b27d7b768b129037442248cc6f7
OMAR-ALshalq <omaralshalq@gmail.com>
2026-03-07 23:12:19 +0300

    edit url imag FeaturedPropertyTypes

--- src/Commponent/featuredpropertytypes/FeaturedPropertyTypes.jsx
+++ src/Commponent/featuredpropertytypes/FeaturedPropertyTypes.jsx
@@ -4,31 +4,31 @@ import "./FeaturedPropertyTypes.css"
 const ArrayItems = [
   {
     id: 1,
-    img: "../../../public/images/hero/h1.png",
+    img: "/images/hero/h1.png",
     title: "Family House",
     dis: "122 Property"
   },
   {
     id: 2,
-    img: "../../../public/images/hero/h2.png",
+    img: "/images/hero/h2.png",
     title: "House & Villa",
     dis: "155 Property"
   },
   {
     id: 3,
-    img: "../../../public/images/hero/h3.png",
+    img: "/images/hero/h3.png",
     title: "Apartment",
     dis: "300 Property"
   },
   {
     id: 4,
-    img: "../../../public/images/hero/h4.png",
+    img: "/images/hero/h4.png",
     title: "Office & Studio",
     dis: "80 Property"
   },
   {
     id: 5,
-    img: "../../../public/images/hero/h6.png",
+    img: "/images/hero/h6.png",
     title: "Villa & Condo",
     dis: "80 Property"
   }
